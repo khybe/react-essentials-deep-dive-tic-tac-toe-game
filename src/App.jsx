bf22f876061e791be95eb2ff4c4777a7a4eda323
Khaibar Saadat <khybersaadat93@gmail.com>
2024-01-25 20:26:28 +0100

    An Alternative To Lifting State Up

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -94,11 +94,13 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="0"
             isActive={activePlayer === "0"}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (
